758c80beb5c40b86b5f32e14ffbe4b5eee0553da
theprick <padrian@gmail.com>
2014-05-01 19:27:36 +0300

    Added P32, compute greatest common divisor. - Added tests. - Renamed i to value. - Added implicit converter. - Added README, githowto.txt.

--- src/main/scala/arithmetic/S99Int.scala
+++ src/main/scala/arithmetic/S99Int.scala
@@ -1,21 +1,29 @@
 package arithmetic
 
 import scala.language.implicitConversions
-import S99Int._
+import scala.annotation.tailrec
 
-class S99Int(val i: Int) {
+class S99Int(val value: Int) {
 
   /**
    * http://en.wikipedia.org/wiki/Primality_test
    */
   def isPrime: Boolean =
-    (i > 1) && (Stream.cons(2, Stream(3, 2)) takeWhile ( _ <= Math.sqrt(i)) forall ( i % _ > 0 ))
+    (value > 1) && (Stream.cons(2, Stream(3, 2)) takeWhile ( _ <= Math.sqrt(value)) forall ( value % _ > 0 ))
 }
 
 object S99Int {
   implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
 
+  implicit def s99Int2Int(i: S99Int): Int = i.value
+
   def apply(i: Int) = new S99Int(i)
 
   val primes = Stream.cons(2, Stream(3, 2)) filter ( _.isPrime )
+
+  /**
+   * P32 (**) Determine the greatest common divisor of two positive integer numbers.
+   * http://en.wikipedia.org/wiki/Greatest_common_divisor
+   */
+  @tailrec def gcd(x: Int, y: Int): Int = if (y == 0) x else gcd(y, x % y)
 }
--- src/test/scala/arithmetic/S99IntTestSuite.scala
+++ src/test/scala/arithmetic/S99IntTestSuite.scala
@@ -1,4 +1,4 @@
-package arithmetics
+package arithmetic
 
 import arithmetic.S99Int._
 import org.scalatest.FunSuite
@@ -7,12 +7,35 @@ import org.scalatest.FunSuite
  * Created by Adrian on 13.04.2014.
  */
 class S99IntTestSuite extends FunSuite {
-  test("'isPrime' true") {
-      assume(7.isPrime)
+  test("7 is prime") {
+    assume(7.isPrime)
   }
 
-  test("'isPrime' false") {
-      assume(!8.isPrime)
+  test("8 is not prime") {
+    assume(!8.isPrime)
+  }
 
+  test("gdc(48, 18) = 6") {
+    assert(gcd(48, 18) === 6)
+  }
+
+  test("gdc(30, 12) = 6") {
+    assert(gcd(30, 12) === 6)
+  }
+
+  test("gdc(77, 55) = 11") {
+    assert(gcd(77, 55) === 11)
+  }
+
+  test("gdc(77, 2) = 1") {
+    assert(gcd(77, 2) === 1)
+  }
+
+  test("gdc(3, 76) = 1") {
+    assert(gcd(3, 76) === 1)
+  }
+
+  test("gdc(3, 76) = 1 with conversion from S99Int to Int") {
+    assert(gcd(new S99Int(3), 76) === 1)
   }
 }
