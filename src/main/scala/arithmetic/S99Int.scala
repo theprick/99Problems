076a2d3c39dacc1610f0629fc96380af759683fa
theprick <padrian@gmail.com>
2014-05-01 22:19:42 +0300

    Added isComprimeTo(P33) and totient(P34) - Added tests.

--- src/main/scala/arithmetic/S99Int.scala
+++ src/main/scala/arithmetic/S99Int.scala
@@ -2,14 +2,32 @@ package arithmetic
 
 import scala.language.implicitConversions
 import scala.annotation.tailrec
+import arithmetic.S99Int._
 
 class S99Int(val value: Int) {
 
   /**
+   * P31 (**) Determine whether a given integer number is prime.
    * http://en.wikipedia.org/wiki/Primality_test
    */
   def isPrime: Boolean =
-    (value > 1) && (Stream.cons(2, Stream(3, 2)) takeWhile ( _ <= Math.sqrt(value)) forall ( value % _ > 0 ))
+    (value > 1) && (Stream.cons(2, Stream.from(3, 2)) takeWhile (_ <= Math.sqrt(value)) forall (value % _ > 0))
+
+
+  /**
+   * P33 (*) Determine whether two positive integer numbers are coprime.
+   * Two numbers are coprime if their greatest common divisor equals 1
+   */
+  def isComprimeTo(n: Int): Boolean = gcd(value, n) == 1
+
+  /**
+   * P34 (**) Calculate Euler's totient function phi(m).
+   * Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r <= m) that are coprime to m.
+   * http://en.wikipedia.org/wiki/Totient
+   * @return the multitude of numbers less than value, and which have no common divisor with it
+   */
+  def totient: Int = Stream.from(1) takeWhile (_ <= value) count isComprimeTo
+  //(1 to start) filter { start.isCoprimeTo(_) } length
 }
 
 object S99Int {
@@ -19,7 +37,7 @@ object S99Int {
 
   def apply(i: Int) = new S99Int(i)
 
-  val primes = Stream.cons(2, Stream(3, 2)) filter ( _.isPrime )
+  val primes = Stream.cons(2, Stream(3, 2)) filter (_.isPrime)
 
   /**
    * P32 (**) Determine the greatest common divisor of two positive integer numbers.
--- src/test/scala/arithmetic/S99IntTestSuite.scala
+++ src/test/scala/arithmetic/S99IntTestSuite.scala
@@ -8,11 +8,11 @@ import org.scalatest.FunSuite
  */
 class S99IntTestSuite extends FunSuite {
   test("7 is prime") {
-    assume(7.isPrime)
+    assert(7.isPrime)
   }
 
   test("8 is not prime") {
-    assume(!8.isPrime)
+    assert(!8.isPrime)
   }
 
   test("gdc(48, 18) = 6") {
@@ -37,5 +37,17 @@ class S99IntTestSuite extends FunSuite {
 
   test("gdc(3, 76) = 1 with conversion from S99Int to Int") {
     assert(gcd(new S99Int(3), 76) === 1)
+  }
+
+  test("9 and 4 are coprime") {
+    assert(9.isComprimeTo(4))
+  }
+
+  test("21 and 15 are not coprime") {
+    assert(!21.isComprimeTo(15))
+  }
+
+  test("totient of 10 is 4") {
+    assert(10.totient === 4)
   }
 }
